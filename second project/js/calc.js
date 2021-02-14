d76ecf78235673f3f1e7f5d51798434207152a61
ahnyejizz <ahnyejizzang@gmail.com>
2021-02-15 01:39:52 +0900

    두번째 과제 : 계산기 만들기

---
+++ second project/js/calc.js
@@ -0,0 +1,28 @@
+function output(num) {
+        var display = document.getElementById("display");
+        display.value = display.value + num;
+
+    }
+
+    function calc() {
+        var display = document.getElementById("display");
+        var result = eval(display.value);
+        
+        var displayResult = document.getElementById("result");
+        displayResult.value = result;
+
+    }
+
+    function del() {
+        var display = document.getElementById("display");
+        display.value = display.value.substring(0, display.value.length-1);
+        
+    }
+
+    function reset() {
+        var display = document.getElementById("display");
+        display.value="";
+
+        var displayResult = document.getElementById("result");
+        displayResult.value="";
+    }
